f82c21901d00594614295698aa6e23975f30d932
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-07 02:18:44 -0300

    Nível 4

---
+++ Level 4 - Data Binding/4.3 -/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RacesComponent } from './races.component';
+
+@Component({
+  selector: 'racing-app',
+  template: `
+    <h1>{{heading}}</h1>
+    <my-races></my-races>
+  `,
+  directives: [RacesComponent]
+  
+})
+export class AppComponent {
+  heading = "Ultra Racing Schedule"
+}
---
+++ Level 4 - Data Binding/4.3 -/app/races.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Race } from './race';
+import { RACES } from './mocks';
+
+@Component({
+  selector: 'my-races',
+  templateUrl: 'app/races.component.html',
+  styleUrls:['app/races.component.css']
+})
+export class RacesComponent {
+  heading = "Ultra Racing Schedule"
+  cash = 10000;
+  races: Race[];
+
+  ngOnInit() {
+    this.races = RACES;
+  }
+
+  totalCost() {
+    let sum = 0;
+    for (let race of this.races) {
+      if (race.isRacing) sum += race.entryFee;
+    }
+    return sum;
+  }
+
+  cashLeft() {
+    return this.cash - this.totalCost();
+  }
+  
+    enterRace(race) {
+    if (this.cashLeft() > race.entryFee) {
+      race.isRacing = true;
+    } else {
+      alert("You don't have enough cash");
+    }
+  }
+  cancelRace(race) {
+    race.isRacing = false;
+  }
+}
